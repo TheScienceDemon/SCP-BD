b48b181a6a1d8c98e40d405e24dcb6c41d5d653a
TheScienceDemon <louisruppert0@gmail.com>
2021-06-12 21:48:12 +0200

    Scene Quick Loader hinzugefügt

---
+++ SCP Site-19/Assets/_Scripts/Editor/Loader.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+
+public class Loader : EditorWindow
+{
+    [MenuItem("Window/Site19Custom/SQL")]
+    public static void ShowWindow()
+    {
+        GetWindow<Loader>("Scene Quick Loder");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Hauptmenü"))
+        {
+            EditorSceneManager.OpenScene("Assets/_Scenes/MainMenu.unity");
+        }
+
+        if (GUILayout.Button("Einzelspieler"))
+        {
+            EditorSceneManager.OpenScene("Assets/_Scenes/FacilityEinzelspieler.unity");
+        }
+
+        if (GUILayout.Button("Mehrspieler"))
+        {
+            EditorSceneManager.OpenScene("Assets/_Scenes/MehrspielerError.unity");
+        }
+
+        GUILayout.EndHorizontal();
+    }
+}
