2e4e6cbf90ea755e67beaab1c0547f79bcba9759
TheScienceDemon <louisruppert0@gmail.com>
2021-08-25 01:58:27 +0200

    1 Tür + 1 Spielermodell +  2 Räume glasubae ich oder 1

--- SCPBD/Assets/_Scripts/Airlock.cs
+++ SCPBD/Assets/_Scripts/Airlock.cs
@@ -2,18 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Airlock : MonoBehaviour
 {
+    public bool isInteractable;
+    [SerializeField] bool shouldDecont;
     [SerializeField] bool isFrontOpen;
-    [SerializeField] bool usesButton;
 
     [Header("Sounds")]
     [SerializeField] AudioClip DecontGas;
+    [SerializeField] AudioClip DecontComplete;
     [SerializeField] AudioClip[] doorOpen;
     [SerializeField] AudioClip[] doorClose;
 
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     public void UseAirlock()
     {
+        if (shouldDecont)
+        {
+            if (isFrontOpen)
+            {
 
+            }
+        }
     }
 }
--- SCPBD/Assets/_Scripts/Elevator.cs
+++ SCPBD/Assets/_Scripts/Elevator.cs
@@ -6,6 +6,7 @@ public class Elevator : MonoBehaviour
 {
     public bool isInteractable;
     [SerializeField] bool isAtDest;
+    [SerializeField] float elevatorTravelSoundChangeTime;
     [SerializeField] Vector3 positionDifference;
     [SerializeField] List<Transform> objectsToTravel;
 
@@ -23,30 +24,74 @@ public class Elevator : MonoBehaviour
     [SerializeField] AudioClip[] elevatorOpen;
     [SerializeField] AudioClip[] elevatorClose;
 
+    [Header("Elevator Buttons")]
+    [SerializeField] Renderer[] normalButtons;
+    [SerializeField] Renderer[] destButtons;
+
+    [Header("Materials")]
+    [SerializeField] Material buttonOpen;
+    [SerializeField] Material buttonClose;
+    [SerializeField] Material buttonLocked;
+
     void Start()
     {
         if (!isAtDest)
+        {
             normalElevDoor.SetBool("isOpen", true);
+
+            foreach (Renderer button in destButtons)
+                button.material = buttonClose;
+        }
         else
+        {
             destElevDoor.SetBool("isOpen", true);
+
+            foreach (Renderer button in normalButtons)
+                button.material = buttonClose;
+        }
     }
 
     public IEnumerator MoveElevator()
     {
         if (!isAtDest)
         {
-            isInteractable = false;
             int i = Random.Range(0, elevatorClose.Length);
+            isInteractable = false;
+
+            foreach (Renderer button in normalButtons)
+                button.material = buttonClose;
+
             normalElevDoor.GetComponent<AudioSource>().PlayOneShot(elevatorClose[i]);
             normalElevDoor.SetBool("isOpen", false);
             yield return new WaitForSeconds(2.75f);
+
+            foreach (Renderer button in normalButtons)
+                button.material = buttonLocked;
+
+            foreach (Renderer button in destButtons)
+                button.material = buttonLocked;
+
+            normalAudioSource.volume = 1f;
             normalAudioSource.PlayOneShot(elevatorTravel);
-            yield return new WaitForSeconds(elevatorTravel.length - 2.75f);
+            destAudioSource.volume = .1f;
+            destAudioSource.PlayOneShot(elevatorTravel);
+            yield return new WaitForSeconds((elevatorTravel.length / 2) - elevatorTravelSoundChangeTime);
 
             foreach (Transform objectToTravel in objectsToTravel)
                 objectToTravel.position -= positionDifference;
 
+            normalAudioSource.volume = 0.1f;
+            destAudioSource.volume = 1f;
+            yield return new WaitForSeconds((elevatorTravel.length / 2) - 2f + elevatorTravelSoundChangeTime);
+
             destAudioSource.PlayOneShot(elevatorReachDest);
+
+            foreach (Renderer button in normalButtons)
+                button.material = buttonClose;
+
+            foreach (Renderer button in destButtons)
+                button.material = buttonOpen;
+
             destElevDoor.GetComponent<AudioSource>().PlayOneShot(elevatorOpen[i]);
             destElevDoor.SetBool("isOpen", true);
             isAtDest = true;
@@ -55,18 +100,43 @@ public class Elevator : MonoBehaviour
         }
         else
         {
-            isInteractable = false;
             int i = Random.Range(0, elevatorClose.Length);
+            isInteractable = false;
+
+            foreach (Renderer button in destButtons)
+                button.material = buttonClose;
+
             destElevDoor.GetComponent<AudioSource>().PlayOneShot(elevatorClose[i]);
             destElevDoor.SetBool("isOpen", false);
             yield return new WaitForSeconds(2.75f);
+
+            foreach (Renderer button in normalButtons)
+                button.material = buttonLocked;
+
+            foreach (Renderer button in destButtons)
+                button.material = buttonLocked;
+
+            destAudioSource.volume = 1f;
             destAudioSource.PlayOneShot(elevatorTravel);
-            yield return new WaitForSeconds(elevatorTravel.length - 2.75f);
+            normalAudioSource.volume = .1f;
+            normalAudioSource.PlayOneShot(elevatorTravel);
+            yield return new WaitForSeconds((elevatorTravel.length / 2) - elevatorTravelSoundChangeTime);
 
             foreach (Transform objectToTravel in objectsToTravel)
                 objectToTravel.position += positionDifference;
 
+            destAudioSource.volume = 0.1f;
+            normalAudioSource.volume = 1f;
+            yield return new WaitForSeconds((elevatorTravel.length / 2) - 2f + elevatorTravelSoundChangeTime);
+
             normalAudioSource.PlayOneShot(elevatorReachDest);
+
+            foreach (Renderer button in destButtons)
+                button.material = buttonClose;
+
+            foreach (Renderer button in normalButtons)
+                button.material = buttonOpen;
+
             normalElevDoor.GetComponent<AudioSource>().PlayOneShot(elevatorOpen[i]);
             normalElevDoor.SetBool("isOpen", true);
             isAtDest = false;
