e0ea668079c80db8927eac9253ed8af652224949
TheScienceDemon <louisruppert0@gmail.com>
2021-10-15 02:07:48 +0200

    more work on the main menu

--- SCP - The Breach Day/Assets/_Scripts/MainMenuScript.cs
+++ SCP - The Breach Day/Assets/_Scripts/MainMenuScript.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MainMenuScript : MonoBehaviour
 {
+    [SerializeField] TMP_Text infoText;
+    void Start()
+    {
+        infoText.text = "SCP BD " + Application.version + "\n" + "Running on Unity " + Application.unityVersion;
+    }
+
     public void ExitGame()
     {
         Application.Quit();
---
+++ SCP - The Breach Day/Assets/_Scripts/UrlOpener.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UrlOpener : MonoBehaviour
+{
+    [SerializeField] string url;
+
+    public void OpenUrl()
+    {
+        Application.OpenURL(url);
+    }
+}
