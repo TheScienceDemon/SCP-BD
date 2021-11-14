b95fc5ffa19bd4d533b32aff6f775834f8a83ffa
TheScienceDemon <louisruppert0@gmail.com>
2021-11-14 19:46:29 +0100

    added DevTools.cs Script für certain tools

--- SCP - The Breach Day/Assets/_Scripts/Editor/DevTools.cs
+++ SCP - The Breach Day/Assets/_Scripts/Editor/DevTools.cs
@@ -1,11 +1,30 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DevTools
 {
-    [MenuItem("DevTools/Runtime/DisposeDiscordPresence")]
+    [MenuItem("DevTools/DisposeDiscordPresence")]
     public static void DisposeDiscordPresence()
     {
         Debug.Log("Method doesn't work yet");
+    }
+
+    [MenuItem("DevTools/LoadScene/Intro")]
+    public static void LoadSceneIntro()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    [MenuItem("DevTools/LoadScene/MainMenu")]
+    public static void LoadSceneMainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    [MenuItem("DevTools/LoadScene/Facility")]
+    public static void LoadSceneFacility()
+    {
+        SceneManager.LoadScene(2);
     }
 }
