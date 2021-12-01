254674a778065c3b7946a6710f764a369b9ac2df
TheScienceDemon <louisruppert0@gmail.com>
2021-12-01 19:48:50 +0100

    forgot to tick files smh

---
+++ SCP - The Breach Day/Assets/_Scripts/PlayerInterface.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInterface : MonoBehaviour
+{
+    [SerializeField] PlayerStats playerStats;
+    [SerializeField] Slider healthSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupHealth(playerStats.maxHealth);
+    }
+
+    void SetupHealth(int newMaxHealth)
+    {
+        healthSlider.maxValue = newMaxHealth;
+
+    }
+
+    public void SetHealth(int newHealth)
+    {
+
+    }
+}
