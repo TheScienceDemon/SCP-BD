252bc6ba44e0cbcdd1440375919703835e0288bb
TheScienceDemon <louisruppert0@gmail.com>
2021-12-24 00:30:42 +0100

    updated UI in Facility.unity

--- SCP - The Breach Day/Assets/_Scripts/PlayerInterface.cs
+++ SCP - The Breach Day/Assets/_Scripts/PlayerInterface.cs
@@ -8,14 +8,24 @@ public class PlayerInterface : MonoBehaviour
     [SerializeField] PlayerStats playerStats;
     [SerializeField] Slider healthSlider;
 
+    float lerpedHealth;
+
     // Start is called before the first frame update
     void Start()
     {
         SetupHealth(playerStats.maxHealth);
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        lerpedHealth = Mathf.Lerp(lerpedHealth, playerStats.currentHealth, Time.deltaTime * 3f);
+        healthSlider.value = lerpedHealth;
+    }
+
     void SetupHealth(int newMaxHealth)
     {
+        lerpedHealth = newMaxHealth;
         healthSlider.maxValue = newMaxHealth;
         SetHealth(newMaxHealth);
     }
