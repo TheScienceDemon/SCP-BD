ef232ed9702d63166bd24afa5dde2668bd45453c
TheScienceDemon <louisruppert0@gmail.com>
2021-12-30 12:15:10 +0100

    changed how the StructManager works

--- SCP - The Breach Day/Assets/_Scripts/StructManager.cs
+++ SCP - The Breach Day/Assets/_Scripts/StructManager.cs
@@ -2,32 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StructManager
+[System.Serializable]
+public struct Offset
 {
-    [System.Serializable]
-    public struct Offset
-    {
-        public Vector3 position;
-        public Vector3 rotation;
-        public Vector3 scale;
-    }
+    public Vector3 position;
+    public Vector3 rotation;
+    public Vector3 scale;
+}
 
-    public enum AccessTypes
-    {
-        Button,
-        Containment1,
-        Containment2,
-        Containment3,
-        Containment4,
-        Containment5,
-        Administration1,
-        Administration2,
-        Administration3,
-        Administration4,
-        Armory1,
-        Armory2,
-        Armory3,
-        Maintenance,
-        NoEntry
-    };
+public enum AccessTypes
+{
+    Button,
+    Containment1,
+    Containment2,
+    Containment3,
+    Containment4,
+    Containment5,
+    Administration1,
+    Administration2,
+    Administration3,
+    Administration4,
+    Armory1,
+    Armory2,
+    Armory3,
+    Maintenance,
+    NoEntry
+};
+
+public enum SceneIndexes
+{
+    Startup,
+    MainMenu,
+    Facility
 }
